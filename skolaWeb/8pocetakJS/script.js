a9685b1cc4438e54a09b5114cf202f85d7f45614
nikolamanic <nikolamanic24@gmail.com>
2025-12-30 22:44:44 +0100

    Dodajem folder webSkola sa podfolderom 8pocetakJS

---
+++ skolaWeb/8pocetakJS/script.js
@@ -0,0 +1,4 @@
+x = parseInt(prompt('Ceo broj:'));
+y = parseFloat(prompt('Decimalni broj:'));
+z = x + y;
+alert('Zbir brojeva je: ' + z);
---
+++ skolaWeb/8pocetakJS/script2.js
@@ -0,0 +1,6 @@
+a = parseInt(prompt('Prvi broj:'));
+b = parseInt(prompt('Drugi broj:'));
+o = (2*a) + (2*b);
+p = a*b;
+alert('Obim pravougaonika je:' +o);
+alert('Povrsina pravougaonika je:' +p);
